e1e29f95006b0a6acd5c0770d0b22f22d463fc5c
Felix <tinywritingmakesmailhardtoread@gmail.com>
2017-10-22 13:46:23 +0200

    Fixed requirements for pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from distutils.core import setup
 
 setup(
-    name='tf_imagefeeder',
+    name='tfimageutils',
     version='0.1',
     packages=['tfimageutils'],
     url='https://github.com/MrTrustworthy/tfimageutils',
     download_url='https://github.com/MrTrustworthy/tfimageutils/archive/0.1.tar.gz',
     license='MIT',
+    install_requires=['matplotlib', 'numpy'],
     author='MrTrustworthy',
     author_email='tinywritingmakesmailhardtoread@gmail.com',
     description='A utility package for dealing with image inputs into Tensorflow',
