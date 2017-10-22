fe61e11a978fafdc0dce75db08121907b2a14e32
Felix <tinywritingmakesmailhardtoread@gmail.com>
2017-10-22 15:07:34 +0200

    Fix for PyPI

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='tfimageutils',
-    version='0.1.1',
+    version='0.1.3',
     packages=['tfimageutils'],
     url='https://github.com/MrTrustworthy/tfimageutils',
-    download_url='https://github.com/MrTrustworthy/tfimageutils/archive/0.1.1.tar.gz',
+    download_url='https://github.com/MrTrustworthy/tfimageutils/archive/0.1.3.tar.gz',
     license='MIT',
     install_requires=['matplotlib', 'numpy'],
     author='MrTrustworthy',
